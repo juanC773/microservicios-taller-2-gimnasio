4cade6bee77420bebf8f174026509cd4eeb6c85a
juanC773 <115520140+juanC773@users.noreply.github.com>
2026-02-23 18:38:21 -0500

    remove: monolito

--- monilito-gimnasio-mcrs/src/main/java/co/analisys/gimnasio/repository/MiembroRepository.java
+++
@@ -1,7 +0,0 @@
-package co.analisys.gimnasio.repository;
-
-import co.analisys.gimnasio.model.Miembro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MiembroRepository extends JpaRepository<Miembro, Long> {
-}
